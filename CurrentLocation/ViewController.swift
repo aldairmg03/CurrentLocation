22325fe04854d0b6d06febd2f62cdabee69fc424
Aldair Martinez <aldair.garcia@kavak.com>
2022-10-07 15:50:08 -0500

    :sparkles: (location): Get current location
    Get current location with CLLocationManager

--- CurrentLocation/ViewController.swift
+++ CurrentLocation/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
     }
 
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedAlways
+            || manager.authorizationStatus == .authorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("Location: \(locValue.latitude),\(locValue.longitude)")
+    }
+}
